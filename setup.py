e3facce17c33f03f1f9b07e73a45c03cb35b9ddf
deepduke <runningwater.jc@gmail.com>
2023-07-25 18:31:11 +0800

    add setup.py for wheel

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ import setuptools
 
 
 setuptools.setup(
-    name='DORF',
+    name='dorf',
     version='0.0.1',
     description='DORF: Dynamic Object Removal Framework',
-    packages=['src', 'src.filters', 'src.utils'],
+    packages=['dorf', 'dorf.filters', 'dorf.utils'],
     author='deepduke',
-    setup_requires=['wheel']
+    setup_requires=[
+        'wheel',
+        'numpy',
+        'scikit-learn',
+        'pypcd>=0.1.1',
+        'bresenham>=0.2'
+        ]
     )
         
